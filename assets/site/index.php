3716fd51f0030bddd2e3e80da8d22dad23361f2f
RookGit <rookcorp@yandex.ru>
2019-02-17 21:03:16 +0400

    dev login tpl 3

--- assets/site/index.php
+++ assets/site/index.php
@@ -1,6 +1,35 @@
 <?
-    if($url[0] == 'root')
-    {
-        echo $twig->render('login.html');
-    }
+
+$header_menu = [];
+$blocks = [];
+$params = [];
+
+if ($system['user']->auth == 0) {
+    $header_menu[] = [
+        'name' => 'Вход',
+        'url' => '/login'
+    ];
+} else {
+    $header_menu[] = [
+        'name' => 'Личный кабинет',
+        'url' => '/cabinet'
+    ];
+
+    $header_menu[] = [
+        'name' => 'Выход',
+        'url' => '/?action=logout'
+    ];
+
+}
+
+if ($url[0] == 'login') {
+
+    $blocks[] = ['path' => 'login_form.html'];
+}
+
+$params['menu'] = $header_menu;
+$params['url'] = $url;
+$params['blocks'] = $blocks;
+
+echo $twig->render('login.html', $params);
 ?>
