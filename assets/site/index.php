c3bd78307baa143263650ca9ef8f781381c1f1de
RookGit <rookcorp@yandex.ru>
2019-02-19 10:35:08 +0400

    dev js scripts + headers

--- assets/site/index.php
+++ assets/site/index.php
@@ -5,26 +5,22 @@ $blocks = [];
 $params = [];
 
 if ($system['user']->auth == 0) {
-    $header_menu[] = [
-        'name' => 'Вход',
-        'url' => '/login'
-    ];
+    $header_menu[] = ['name' => 'Вход','url' => '/log-in'];
+    $header_menu[] = ['name' => 'Регистрация','url' => '/sign-up'];
+    $header_menu[] = ['name' => 'Забыли пароль?','url' => '/forgot-password'];
 } else {
-    $header_menu[] = [
-        'name' => 'Личный кабинет',
-        'url' => '/cabinet'
-    ];
-
-    $header_menu[] = [
-        'name' => 'Выход',
-        'url' => '/?action=logout'
-    ];
-
+    $header_menu[] = ['name' => 'Личный кабинет','url' => '/cabinet'];
+    $header_menu[] = ['name' => 'Выход','url' => '/?action=logout'];
 }
 
-if ($url[0] == 'login') {
-
+if ($url[0] == 'log-in' || $url[0] == 'root') {
     $blocks[] = ['path' => 'login_form.html'];
+}
+else if ($url[0] == 'sign-up') {
+    $blocks[] = ['path' => 'sign_up_form.html'];
+}
+else if ($url[0] == 'forgot-password') {
+    $blocks[] = ['path' => 'forgot_password_form.html'];
 }
 
 $params['menu'] = $header_menu;
@@ -32,4 +28,11 @@ $params['url'] = $url;
 $params['blocks'] = $blocks;
 
 echo $twig->render('login.html', $params);
-?>
+
+//$res = Authorization::reg_user([
+//    'login' => 'RookDev',
+//    'password' => 'qweqwe22',
+//    'name' => 'Грачев',
+//    'surname' => 'Григорий',
+//    'patronymic' => 'Денисович'
+//]);
--- index.php
+++ index.php
@@ -5,10 +5,10 @@ require_once 'vendor/autoload.php';
 $config['debug'] = true;
 
 // ROOT сайта
-$config['path']['root'] = getcwd().'/';
+$config['path']['root'] = getcwd() . '/';
 
 // Папка с модулями
-$config['path']['modules'] = getcwd().'/../modules/';
+$config['path']['modules'] = getcwd() . '/../modules/';
 
 $config['db']['db'] = 'acod';
 $config['db']['host'] = '127.0.0.1';
@@ -18,13 +18,25 @@ $config['db']['type'] = 'pgsql';
 
 // Подключаемые модули
 $config['modules'] = [
-    '_Database' => ['version' => '1.0'],
-    '_Authorization' => ['version' => '1.0'],
-    'Tpl' => ['version' => '1.0'],
+    '_Database' => ['version' => '1.0', 'scripts' => false],
+    '_Authorization' => ['version' => '1.0', 'scripts' => false],
+    'Tpl' => ['version' => '1.0', 'scripts' => false],
+    'MultiValidator' => ['version' => '1.0'],
 ];
 
 // Ядро
-include_once $config['path']['modules'].'_Core/1.0/Core.php';
+include_once $config['path']['modules'] . '_Core/1.0/Core.php';
+
+
+// Файл для пользовательских php скриптов
+if ($config['scripts_mode'] == false && $config['api_mode'] == false) {
+// Добавляем данные пользователя для бд
+    $system['user']->add_field_site(['name', 'surname', 'patronymic']);
+    require_once $config['path']['root'] . 'assets/site/index.php';
+}
+
 
 //ob_start();
 //ob_get_clean();
+
+$pdo = null;
